aff714f04ae0b856a92fdf6f5b999559c986befc
SandroReller <162469521+SandroReller@users.noreply.github.com>
2025-06-08 19:59:01 +0200

    Handler-has-to-react-on-submit

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,28 +1,34 @@
 import {useState} from "react";
 import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
-
-import formelrad from "../image/formelradelektronik.gif";
 import InputField from "../formular/InputField";
 
 export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
         i: 2,
-        r: ""
+        r: "",
+        p: ""
     })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("handleSubmit")
+    }
 
     return (
         <>
-            <h2>Formelrad</h2>
-            <img src={formelrad} width="200" alt="Formelrad"/>
-            <form>
-                <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
-                <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
-                <button type="submit">Calculate</button>
-            </form>
-
+            <section>
+                <header>
+                    <h2>Formelrad</h2>
+                    <img src={formelrad} width="200" alt="Formelrad"/>
+                </header>
+                <form onSubmit={handleSubmit}>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <button type="submit">Calculate</button>
+                </form>
+            </section>
         </>
     )
 }
